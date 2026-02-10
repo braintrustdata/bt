9924cfc8eab32a220597e53e762e0efe3e1f8a27
Parker Henderson <parker@parkerhendo.com>
2026-02-09 17:18:31 -0800

    test: add unit tests for args, config, and status modules

--- src/args.rs
+++ src/args.rs
@@ -60,3 +60,84 @@ impl<T: Args> CLIArgs<T> {
         (self.base.with_config_defaults(cfg), self.args)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn empty_args() -> BaseArgs {
+        BaseArgs {
+            json: false,
+            org: None,
+            project: None,
+            api_key: None,
+            api_url: None,
+            app_url: None,
+            env_file: None,
+        }
+    }
+
+    fn config(org: Option<&str>, project: Option<&str>) -> Config {
+        Config {
+            org: org.map(String::from),
+            project: project.map(String::from),
+            ..Default::default()
+        }
+    }
+
+    #[test]
+    fn empty_args_filled_from_config() {
+        let args = empty_args();
+        let cfg = config(Some("cfg-org"), Some("cfg-proj"));
+
+        let result = args.with_config_defaults(&cfg);
+
+        assert_eq!(result.org, Some("cfg-org".into()));
+        assert_eq!(result.project, Some("cfg-proj".into()));
+    }
+
+    #[test]
+    fn existing_args_not_overwritten() {
+        let args = BaseArgs {
+            org: Some("cli-org".into()),
+            project: Some("cli-proj".into()),
+            ..empty_args()
+        };
+        let cfg = config(Some("cfg-org"), Some("cfg-proj"));
+
+        let result = args.with_config_defaults(&cfg);
+
+        assert_eq!(result.org, Some("cli-org".into()));
+        assert_eq!(result.project, Some("cli-proj".into()));
+    }
+
+    #[test]
+    fn partial_fill_org_set_project_from_config() {
+        let args = BaseArgs {
+            org: Some("cli-org".into()),
+            project: None,
+            ..empty_args()
+        };
+        let cfg = config(Some("cfg-org"), Some("cfg-proj"));
+
+        let result = args.with_config_defaults(&cfg);
+
+        assert_eq!(result.org, Some("cli-org".into()));
+        assert_eq!(result.project, Some("cfg-proj".into()));
+    }
+
+    #[test]
+    fn empty_config_leaves_args_unchanged() {
+        let args = BaseArgs {
+            org: Some("cli-org".into()),
+            project: None,
+            ..empty_args()
+        };
+        let cfg = config(None, None);
+
+        let result = args.with_config_defaults(&cfg);
+
+        assert_eq!(result.org, Some("cli-org".into()));
+        assert_eq!(result.project, None);
+    }
+}
