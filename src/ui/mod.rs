9f6e781938c333544fccd65f832e9f63a96249f7
Cédric Halber <cedric@braintrustdata.com>
2026-04-03 00:06:39 +0000

    chore: make behavior more consistent and logical

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -10,13 +10,8 @@ mod spinner;
 mod status;
 mod table;
 
-static NO_INPUT: AtomicBool = AtomicBool::new(false);
 static QUIET: AtomicBool = AtomicBool::new(false);
 static ANIMATIONS_ENABLED: AtomicBool = AtomicBool::new(true);
-
-pub fn set_no_input(val: bool) {
-    NO_INPUT.store(val, Ordering::Relaxed);
-}
 
 pub fn set_quiet(val: bool) {
     QUIET.store(val, Ordering::Relaxed);
@@ -47,7 +42,7 @@ pub fn can_prompt() -> bool {
 }
 
 pub fn is_interactive() -> bool {
-    std::io::stdin().is_terminal() && !NO_INPUT.load(Ordering::Relaxed)
+    std::io::stdin().is_terminal()
 }
 
 pub use pager::print_with_pager;
